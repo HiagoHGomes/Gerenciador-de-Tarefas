150b58733323a689de6bc104b515a9899d6bafa4
Hiago Gomes <hiago_h3@hotmail.com>
2023-03-27 18:14:41 -0300

    Conexão com o banco de dados.

--- TodoApp/app/src/main/java/TodoApp/Main.java
+++ TodoApp/app/src/main/java/TodoApp/Main.java
@@ -4,6 +4,9 @@
  */
 package TodoApp;
 
+import java.sql.Connection;
+import util.ConnectionFactory;
+
 /**
  *
  * @author hiago
@@ -11,5 +14,7 @@ package TodoApp;
 public class Main {
     public static void main (String [] args){
         System.out.println("Hello World!");
+        Connection c = ConnectionFactory.getConnection();
+        ConnectionFactory.closeConnection(c);
     }
 }
